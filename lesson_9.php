91abf23106682f471aa4f3ba4fd73342a11bb961
YelyelyeL2023 <148562614+YelyelyeL2023@users.noreply.github.com>
2025-06-13 23:38:59 +0500

    Create lesson_9.php

---
+++ lesson_9.php
@@ -0,0 +1,11 @@
+<?php
+$number1 = 10;
+$number2 = 20;
+
+if ($number1 > $number2) {
+    echo "Первое число больше второго.";
+} elseif ($number1 < $number2) {
+    echo "Второе число больше первого.";
+} else {
+    echo "Оба числа равны.";
+}
