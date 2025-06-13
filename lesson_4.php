e6de85d1610ed91d1fbc89ff0d1512c6b7084d93
YelyelyeL2023 <148562614+YelyelyeL2023@users.noreply.github.com>
2025-06-13 23:15:47 +0500

    Create lesson_4.php

---
+++ lesson_4.php
@@ -0,0 +1,21 @@
+<?php
+// 1.
+$studentName;
+$studentAge;
+$studentAverageScore;
+// 2.
+$carBrand;
+$carMadeIn;
+$carMileage;
+// 3.
+$productName;
+$productAmount;
+$productPrice;
+// 4.
+$userName;
+$userMail;
+$userBirthDate;
+// 5.
+$productName;
+$productCategory;
+$productOrigin;
